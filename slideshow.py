62747816a48cc5d76df6658ba3057efe8574c6d5
Pierre Quentel <pierre.quentel@orange.com>
2017-03-21 18:48:08 +0100

    Remove useless function _keydown ; don't import time, use window.Date instead ; don't capture key events other than up / down / right / left ; use a cookie to open at last visited page on reload ; special highlighting for interactive interpreter snippets.

--- slideshow.py
+++ slideshow.py
@@ -1,33 +1,28 @@
-from browser import document, markdown, html
-import time
+from browser import document, html, window
+import markdown
 import highlight
 
-def _keydown(ev, path, zone, page):
-    if ev.keyCode in [32,39,40]: # key space, right or down : next page
-        show(path, zone, page+1)
-        ev.preventDefault()    
-    elif ev.keyCode in [37,38]: #key left or up: previous page
-        show(path, zone, page-1)
-        ev.preventDefault()
-
 def keydown(ev, slideshow, zone):
-    if ev.keyCode in [32,39,40]: # key space, right or down : next page
+    if ev.keyCode in [39, 40]: # key right or down : next page
         slideshow.page_num += 1
         if slideshow.page_num >= len(slideshow.pages):
             slideshow.page_num = 0
-    elif ev.keyCode in [37,38]: #key left or up: previous page
+        show_page(slideshow, zone, slideshow.page_num)
+        ev.preventDefault()  
+    elif ev.keyCode in [37, 38]: #key left or up: previous page
         slideshow.page_num -= 1
         if slideshow.page_num < 0:
-            slideshow.page_num = len(slideshow.pages)-1
-    show_page(slideshow, zone, slideshow.page_num)
-    ev.preventDefault()  
+            slideshow.page_num = len(slideshow.pages) - 1
+        show_page(slideshow, zone, slideshow.page_num)
+        ev.preventDefault()  
 
 def move_to(ev, slideshow, zone):
-    pc = (ev.x-ev.target.abs_left)/ev.target.width
-    nb_pages = len(slideshow.pages)-1
-    page = round(nb_pages*pc)
+    pc = (ev.x - ev.target.abs_left) / ev.target.width
+    nb_pages = len(slideshow.pages) - 1
+    page = round(nb_pages * pc)
     slideshow.page_num = page
-    new_pos = '%spx' %(ev.x-ev.target.abs_left-(document['tl_pos'].width/2))
+    new_pos = '%spx' %(ev.x  -ev.target.abs_left - 
+        (document['tl_pos'].width / 2))
     # show page at specified position
     show_page(slideshow, zone, page)
     # set new cursor position
@@ -40,7 +35,8 @@ def click_on_tl_pos(ev):
 class Slideshow:
 
     def __init__(self, path):
-        self.src = src = open(path+'?foo=%s' %time.time()).read()
+        qs = window.Date.new().getTime()
+        self.src = src = open(path+'?foo=%s' %qs).read()
         self.title = ''
         self.show_page_num = False
         
@@ -50,14 +46,14 @@ class Slideshow:
         # directives for the document
         while src.startswith('@'):
             line_end = src.find('\n')
-            key,value = src[:line_end].split(' ',1)
+            key,value = src[:line_end].split(' ', 1)
             if key=='@title':
                 self.title = value
             elif key=='@pagenum':
                 self.show_page_num = True
             elif key=="@index":
                 self.contents.append([value, 0])
-            src = src[line_end+1:]
+            src = src[line_end + 1:]
 
         self.pages = []
         lines = []
@@ -68,24 +64,34 @@ class Slideshow:
             elif line.startswith('@pause'):
                 self.pages.append('\n'.join(lines))
             elif line.startswith('@index'):
-                self.contents.append([line.split(' ',1)[1], len(self.pages)])
+                self.contents.append([line.split(' ', 1)[1], len(self.pages)])
             else:
                 lines.append(line)
 
         if lines:
             self.pages.append('\n'.join(lines))
             
-def show(path, zone, page_num=0):
+def show(path, zone, page_num=None):
     slideshow = Slideshow(path)
     
-    if page_num<0:
+    if page_num is None:
+        page_num = 0
+        # check if page num was stored in a cookie
+        cookies = document.cookie
+        if cookies:
+            elts = dict([x.strip() for x in cookie.split('=')] 
+                for cookie in cookies.split(";"))
+            if "page" in elts:
+                page_num = int(elts["page"])
+    if page_num < 0:
         page_num = 0
     elif page_num >= len(slideshow.pages):
-        page_num = len(pages)-1
+        page_num = len(slideshow.pages) - 1
+
     slideshow.page_num = page_num
     document.unbind('keydown')
     document.bind('keydown',lambda ev:keydown(ev, slideshow, zone))
-
+    
     show_page(slideshow, zone, page_num)
 
 def run_code(ev):
@@ -102,13 +108,17 @@ def show_page(slideshow, zone, page_num):
                 selected=page_num>=content[1])
 
     slideshow.page_num = int(page_num)
+    
+    # store page num in a cookie
+    document.cookie = "page={}".format(page_num)
+
     zone.clear()
-            
+    
     body = html.DIV()
     body.html = markdown.mark(slideshow.pages[page_num])[0]
     
     if slideshow.contents:
-        body = html.DIV(toc+body)
+        body = html.DIV(toc + body)
 
     footer = html.DIV(Id="footer")
     if slideshow.title:
@@ -121,10 +131,14 @@ def show_page(slideshow, zone, page_num):
     timeline <= tl_pos
     timeline.bind('click', lambda ev:move_to(ev, slideshow, zone))
     tl_pos.bind('click', click_on_tl_pos)
-    zone <= body+footer+timeline
+    zone <= body + footer +timeline
+    wh = window.innerHeight
+    footer.style.top = "{}px".format(int(wh * 0.9))
+    timeline.style.top = "{}px".format(int(wh * 0.85))
     tl_pos.style.left = '%spx' %(timeline.width*page_num/len(slideshow.pages))
+    document["cours"].style.minHeight = "{}px".format(int(wh * 0.8))
     
-    for elt in zone.get(selector='.python')+zone.get(selector='.marked'):
+    for elt in zone.get(selector='.python'):
         src = elt.text.strip()
         width = max(len(line) for line in src.split('\n'))
         width = max(width, 30)
@@ -132,3 +146,36 @@ def show_page(slideshow, zone, page_num):
         elt.html = highlight.highlight(src).html
         elt.style.width = '%sem' %int(0.7*width)
         elt.bind('click', run_code)
+
+    for elt in zone.get(selector='.python-console'):
+        src = elt.text.strip()
+        lines = src.split('\n')
+        result = ''
+        py = ''
+        py_starts = []
+        for line in lines:
+            if line.startswith('>>>') or line.startswith('...'):
+                py += line[4:]+'\n'
+                py_starts.append('<span class="python-prompt">{}</span>'.format(line[:3]))
+            else:
+                if py:
+                    colored = highlight.highlight(py).html
+                    colored_lines = colored.split('\n')
+                    if result:
+                        result += '\n'
+                    result += '\n'.join(start+' '+line
+                        for (start, line) in zip(py_starts, colored_lines))
+                    py = ''
+                    py_starts = []
+                result += '\n' + line
+        if py:
+            colored = highlight.highlight(py).html
+            colored_lines = colored.split('\n')
+            if result:
+                result += '\n'
+            result += '\n'.join(start+' '+line
+                for (start, line) in zip(py_starts, colored_lines))
+            py = ''
+            py_starts = []
+
+        elt.html = result
